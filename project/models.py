619a2d3ae47411f0280bd16c8fc0c5a31b3e7237
nandom yabal <yabalmicheal737@gmail.com>
2020-02-06 11:44:12 +0000

    made some update in flieds

--- project/models.py
+++ project/models.py
@@ -13,8 +13,8 @@ class Intern(db.Model):
     duration = db.Column(db.String(15), nullable=False)
     email = db.Column(db.String(60), unique=True, nullable=False)
     message = db.Column(db.Text, nullable=False)
-    int_date = db.Column(db.Date, nullable=False)
-    int_time = db.Column(db.Time, nullable=False)
+    int_date = db.Column(db.Date, unique=True, nullable=False)
+    int_time = db.Column(db.Time, unique=True, nullable=False)
 
     def __init__(self, name, school, department, phone, duration, email, message, int_date, int_time):
         self.name = name
--- project/views.py
+++ project/views.py
@@ -20,17 +20,21 @@ def is_registered(f):
 @app.route('/', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST' and 'name' in request.form and 'school' in request.form and 'department' in request.form and 'phone' in request.form and 'duration' in request.form and 'email' in request.form and 'message' in request.form and 'int_date' in request.form and 'int_time' in request.form:
-        try:
-            new_intern = Intern(name = request.form['name'], school = request.form['school'], department = request.form['department'], phone = request.form['phone'], duration = request.form['duration'], email = request.form['email'], message = request.form['message'], int_date = request.form['int_date'], int_time = request.form['int_time'])
-            new_intern.authenticated = True
-            db.session.add(new_intern)
-            db.session.commit()
-            session['regin'] = True
-            flash('{} you have been sucessfully registered!'.format(new_intern.name), 'success')
-            return redirect(url_for('intern'))
-        except exc.IntegrityError:
-            db.session.rollback()
-            flash('ERROR! Email {} already exists.'.format(new_intern.email), 'error')
+        nw_time =Intern.query.order_by(Intern.int_time, Intern.int_date).all()
+        if request.form['int_time'] and request.form['int_date'] != nw_time:
+
+            try:
+                new_intern = Intern(name = request.form['name'], school = request.form['school'], department = request.form['department'], phone = request.form['phone'], duration = request.form['duration'], email = request.form['email'], message = request.form['message'], int_date = request.form['int_date'], int_time = request.form['int_time'])
+                new_intern.authenticated = True
+                db.session.add(new_intern)
+                db.session.commit()
+                session['regin'] = True
+                flash('{} you have been sucessfully registered!'.format(new_intern.name), 'success')
+                return redirect(url_for('intern'))
+            except exc.IntegrityError:
+                db.session.rollback()
+                flash('ERROR! Email {} or Time {} already exists.'.format(new_intern.email, new_intern.int_time), 'error')  
+
     return render_template("index.html", name = "Application")
 
 
